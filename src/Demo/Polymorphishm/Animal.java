4a7f2efa95e037601a0e95378aa60fbdb07a5608
YOGESH REWALA <rewalayogesh@gmail.com>
2023-02-02 11:25:24 +0530

    Polymorphism and inheritance demo

---
+++ src/Demo/Polymorphishm/Animal.java
@@ -0,0 +1,9 @@
+package Demo.Polymorphishm;
+
+public class Animal {
+    public void eat(){
+        System.out.println("eating");}
+    public void sleep(){
+        System.out.println("Animal needs sleep");
+    }
+}
